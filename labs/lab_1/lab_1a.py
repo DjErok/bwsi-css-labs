3b2de62449a1c803bdfa233641940245ab984699
DjErok <113222677+DjErok@users.noreply.github.com>
2026-02-18 18:12:22 -0800

    practiceupdate to lab.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Eric Cao" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, I'm Eric, a sophomore from California. I enjoy skating and skiing.")
 
 if __name__ == "__main__":
     main()
